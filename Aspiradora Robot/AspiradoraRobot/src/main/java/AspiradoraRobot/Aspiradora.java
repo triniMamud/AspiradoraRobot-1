3b77374192460276a6770792a80a898d32ed75c4
preadigos <preadigos@yahoo.es>
2020-11-08 13:42:26 -0300

    Agregado de comentarios
    Comentarios agregados.

--- Aspiradora Robot/AspiradoraRobot/src/main/java/AspiradoraRobot/Aspiradora.java
+++ Aspiradora Robot/AspiradoraRobot/src/main/java/AspiradoraRobot/Aspiradora.java
@@ -1,4 +1,23 @@
+/*
+El TP consiste en el diseño de una aspiradora robot. Básicamente su funcionamiento es recorrer un ambiente y limpiarlo. 
+Al encontrar obstáculos debe intentar esquivarlos. Ocasionalmente, puede ser que deje de funcionar porque sus cepillos deben ser limpiados.
+En esencia, se puede resumir el funcionamiento de la aspiradora a través de los siguientes puntos:
+-	Una vez encendida, recorre el ambiente moviéndose  y lo limpia con sus cepillos.
+-	Tiene dos cepillos, izquierdo y derecho.
+-	Se mueve hacia adelante, hacia atrás, hacia la izquierda,  y hacia la derecha.
+-	Si algún cepillo está sucio la aspiradora se apaga e informa el error. Debe manualmente limpiarse y volverla a encender.
+-	Tiene dos modos de velocidad: normal y rápido.  
+        El modo puede cambiarse en todo momento. Con el modo rápido la aspiradora se desplaza con mayor velocidad. 
+-	Una vez que termina de recorrer el ambiente, se apaga.
+-	Si la batería se agota, la aspiradora se apaga e informa el error.
+-	Para cargar la batería, simplemente se conecta manualmente a la corriente.
+-	Ante la presencia de un obstáculo, debe intentar moverse para ver si es posible esquivarlo. De no ser posible, se apaga e informa el error.
 
+Se debe modelar el ambiente a limpiar, y el funcionamiento de la aspiradora.  Se deben entregar el diagrama de clases, 
+y 5 diagramas de secuencia para 5 comportamientos específicos a elección. 
+
+
+*/
 package AspiradoraRobot;
 
 public class Aspiradora {
